971881479adcc3a0d5577ca77cc30a6729bc36a9
Kevin Friday <kevinfriday87@gmail.com>
2022-05-05 18:20:14 -0500

    folder update

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// Save the description in the textarea w/loacal storage
+
+// Load the description in the textarea w/loacal storage on start of page
+
+// dom elements for the save button
+
+//conditonal fucntion evaluating the current time vs the task time "textarea  id"
+// this should run on load
+// use a for loop
+// queryselectall creates an array of item to loop through in the conditional function
+
+// save fucntion that takes the  hour of the as a keyvalue to save in local storage. 
+// save function 
