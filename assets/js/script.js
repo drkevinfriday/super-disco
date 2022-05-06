1e47866e3423ea79650e80954c1f23ab84a5212e
Kevin Friday <kevinfriday87@gmail.com>
2022-05-05 20:21:10 -0500

    Dom element test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,21 @@
+//Dom elements
+var saveBtnEl = document.querySelector(".saveBtn")
+
+
+
+
+
+
+
+var timeData = $(".hour").data().time // this accees the data element in the time-block div
+
+var textdata = $(".description").val() // this is the info in the text area 
+
+
+
+
 // Save the description in the textarea w/loacal storage
+
 
 // Load the description in the textarea w/loacal storage on start of page
 
@@ -11,3 +28,31 @@
 
 // save fucntion that takes the  hour of the as a keyvalue to save in local storage. 
 // save function 
+
+
+// this function gets the hour data from the 
+var getHourFunc = function(){
+    console.log("Time data")
+    console.log(timeData)
+      //returns the time for div being accessed
+    return this.timeData
+    // console.log("This works")
+}
+// this function gets the data from the textarea 
+var getTextFunc = function(){
+    console.log("Text area data")
+    console.log(textdata)
+    //returns the text for div being accessed
+    return this.textdata
+    // console.log("This works")
+}
+
+
+// this runs gethour and gettext
+saveBtnEl.addEventListener("click",() => {
+    getHourFunc();
+    getTextFunc();
+})
+
+
+
