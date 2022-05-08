561bc1c726fcb2ab3c5d099dc7edd117d2642cc6
Kevin Friday <kevinfriday87@gmail.com>
2022-05-08 18:38:58 -0500

    js update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,24 @@
 //Dom elements
-// let saveBtnEl = document.querySelector("#btn9")
-// let saveBtn2El = document.querySelector("#btn10")
+
+// displays the current time rounded down to the hour
+let currentTime = moment().hour()
+
+console.log(currentTime)
+window.onload = function() {
+    checkTime();
+  };
 
 
-// $(document).ready(function(){
-//     console.log(allStorage)
-// })
-
-
-
-// Save the description in the textarea w/loacal storage
+// Save the description in the textarea w/loacal storage as value
+// save fucntion that takes the hour  as a key to save in local storage. 
+// save function 
 var saveData = function (key, value){
     console.log(key,value)
     localStorage.setItem(key,value)
 }
 
 
-// Load the description in the textarea w/loacal storage on start of page
-// var  allStorage= function(){
-
-//     var archive = {}, // Notice change here
-//         keys = Object.keys(localStorage),
-//         i = keys.length;
-
-//     while ( i-- ) {
-//         archive[ keys[i] ] = localStorage.getItem( keys[i] );
-//     }
-
-//     console.log(archive)
-//     return archive;
-
-   
-// }
-
-
-// for(var i = 0, )
-
-
+// Load the description  by key value reference 
 $('#9 .description').val(localStorage.getItem('9'))
 $('#10 .description').val(localStorage.getItem('10'))
 $('#11 .description').val(localStorage.getItem('11'))
@@ -52,24 +34,27 @@ $('#17 .description').val(localStorage.getItem('17'))
 
 
 
-// dom elements for the save button
+
 
 //conditonal fucntion evaluating the current time vs the task time "textarea  id"
 // this should run on load
 // use a for loop
 // queryselectall creates an array of item to loop through in the conditional function
 
-// save fucntion that takes the  hour of the as a keyvalue to save in local storage. 
-// save function 
+//  function checkTime() {
+//     let taskTime = $(".time-block").parent().attr("id");
+//     console.log("taskTime")
+//     console.log(taskTime)
+
+// }
+
 
 
 
 
 
 // pulls the info from the button click and saves it to local storage
-$(".saveBtn").click(function (event)
-
- {
+$(".saveBtn").click(function (event){
     //this pulls the task info from the siblings div of the button pressed
     let taskDescription =$(this).siblings('.description').val()
     
@@ -85,9 +70,7 @@ $(".saveBtn").click(function (event)
     saveData(dataID,taskDescription)
     })
 
-// runs the the handle button once save is pressed
-// saveBtnEl.addEventListener("click", handleBtnClick);
-// saveBtn2El.addEventListener("click", handleBtnClick);
+
 
 
 
